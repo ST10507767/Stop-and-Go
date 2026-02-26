b1ed235194cc50b0f7235f3ffac9c70196af7b98
ST10507767 <ST10507767@myemeris.edu.za>
2026-02-26 12:34:08 +0200

    My first Submission

---
+++ src/classactivity/ClassActivity.java
@@ -0,0 +1,47 @@
+/**
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package classactivity;
+
+/**
+ *
+ * @author ST10507767
+ */
+ 
+ //This code creates a stop and go sign when it is ran.
+public class ClassActivity {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        String A="   ^^^^^^^^            |             ^^^^^^^^";
+        String B=" ^          ^          |           ^          ^";
+        String C="^    STOP    ^         |          ^     GO     ^";
+        String D=" ^          ^          |           ^          ^";
+        String E="   ^^^^^^^^            |             ^^^^^^^^";
+        String F="       ^               |                 ^";
+        String G="       ^               |                 ^";
+        String H="       ^               |                 ^";
+        String I="       ^               |                 ^";
+        String J="    ^^^^^^^     _______________       ^^^^^^^";
+        
+
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+        System.out.println(D);
+        System.out.println(E);
+        System.out.println(F);
+        System.out.println(G);
+        System.out.println(H);
+        System.out.println(I);
+        System.out.println(J);
+       
+        
+    }
+
+}
