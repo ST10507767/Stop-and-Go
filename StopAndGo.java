679610b4735e4760e5da0e086fef01e2ac175cbc
ST10507767 <ST10507767@myemeris.edu.za>
2026-03-04 09:18:46 +0200

    Add files via upload

---
+++ StopAndGo.java
@@ -0,0 +1,32 @@
+s/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.stopandgo;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class StopAndGo {
+
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("               -----------");
+        System.out.println("");
+        System.out.println("     ^^^^^^^^               $$$$$$$$");
+        System.out.println("   ^          ^           $          $");
+        System.out.println("  ^    STOP    ^         $     GO     $");
+        System.out.println("   ^          ^           $          $");
+        System.out.println("     ^^^^^^^^               $$$$$$$$");
+        System.out.println("        ^^                     $$");
+        System.out.println("        ^^                     $$");
+        System.out.println("        ^^                     $$");
+        System.out.println("        ^^                     $$");
+        System.out.println("       ^^^^                   $$$$");
+        System.out.println("    ^^^^^^^^^^             $$$$$$$$$$");
+        System.out.println("|------------------------------------------|");
+        System.out.println("|------------------------------------------|");
+                
+    }
+}
